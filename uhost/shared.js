904f18359db652f2a11499e615b7d6ef1f71366d
Mathew Chong <mathewchong.dev@gmail.com>
2021-05-16 16:29:06 +1000

    Add mobile nav hamburger menu to index page

--- uhost/shared.js
+++ uhost/shared.js
@@ -2,6 +2,8 @@ var backdrop = document.querySelector('.backdrop');
 var modal = document.querySelector('.modal');
 var modalNoButton = document.querySelector('.modal__action--negative');
 var selectPlanButtons = document.querySelectorAll('.plan button');
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav');
 
 for (var i = 0; i < selectPlanButtons.length; i++) {
     selectPlanButtons[i].addEventListener('click', function () {
@@ -10,7 +12,10 @@ for (var i = 0; i < selectPlanButtons.length; i++) {
     })
 }
 
-backdrop.addEventListener('click', closeModal);
+backdrop.addEventListener('click', function () {
+    mobileNav.style.display = 'none';
+    closeModal();
+});
 
 modalNoButton.addEventListener('click', closeModal);
 
@@ -18,3 +23,8 @@ function closeModal() {
     modal.style.display = 'none';
     backdrop.style.display = 'none';
 }
+
+toggleButton.addEventListener('click', function () {
+    mobileNav.style.display = 'block';
+    backdrop.style.display = 'block';
+})
