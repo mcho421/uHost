fb4433d2aca615c9956fccda125f32d879b822d2
Mathew Chong <mathewchong.dev@gmail.com>
2021-05-16 16:50:07 +1000

    Use open class rather than setting styles directly

--- uhost/shared.js
+++ uhost/shared.js
@@ -7,24 +7,24 @@ var mobileNav = document.querySelector('.mobile-nav');
 
 for (var i = 0; i < selectPlanButtons.length; i++) {
     selectPlanButtons[i].addEventListener('click', function () {
-        modal.style.display = 'block';
-        backdrop.style.display = 'block';
+        modal.classList.add('open');
+        backdrop.classList.add('open');
     })
 }
 
 backdrop.addEventListener('click', function () {
-    mobileNav.style.display = 'none';
+    mobileNav.classList.remove('open');
     closeModal();
 });
 
 modalNoButton.addEventListener('click', closeModal);
 
 function closeModal() {
-    modal.style.display = 'none';
-    backdrop.style.display = 'none';
+    modal.classList.remove('open');
+    backdrop.classList.remove('open');
 }
 
 toggleButton.addEventListener('click', function () {
-    mobileNav.style.display = 'block';
-    backdrop.style.display = 'block';
+    mobileNav.classList.add('open');
+    backdrop.classList.add('open');
 })
